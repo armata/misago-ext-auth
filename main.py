41a5ea3eb11fea23dc76e212397a3723a04a8c6a
Sebastian Malek <armata@users.noreply.github.com>
2017-01-17 15:29:12 +0100

    this should work now

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
+# make sure to set SESSION_COOKIE_HTTPONLY to False
+
 import requests
 
 FORUM_URL = 'https://misago-project.org/'
 
-USERNAME = 'username'
-PASSWORD = 'password'
+USERNAME = ''
+PASSWORD = ''
+
+s = requests.Session()
 
 # make a GET request to the token API endpoint
 url = '{}/api/auth/token/'.format(FORUM_URL)
-r = requests.get(url)
+r = s.get(url)
 
 # extract the CSRF token
-csrftoken = r.cookies['csrftoken']
+csrftoken = s.cookies.get('csrftoken')
 
-print(csrftoken)
+# update headers
+s.headers.update({'X-CSRFToken': csrftoken})
 
 # make a POST request to the auth API endpoint
 url = '{}/api/auth/'.format(FORUM_URL)
 data = {'username': USERNAME, 'password': PASSWORD}
-headers = {'X-CSRFToken': csrftoken}
-
-r = requests.post(url, data=data, headers=headers)
-
-print(r.text)
+r = s.post(url, data=data)
